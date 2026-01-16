fdd927b64b5854f3b99be3b49f32f999097abc64
Interstellar-Column <interstellarcolumn@heavenly-cyberspace.xyz>
2026-01-16 00:18:06 +0000

    B: Draw Player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -8,13 +9,18 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-    pygame.Surface.fill("black")
-    pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
